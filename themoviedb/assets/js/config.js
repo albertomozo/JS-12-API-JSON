6a8f3edc219b6011bf5e2dc5eb55c4f861d59d0f
Alberto Mozo <albertomozodocente@gmail.com>
2025-07-29 20:04:04 +0200

    Remove config files from tracking

--- themoviedb/assets/js/config.js
+++
@@ -1,4 +0,0 @@
-/* obten  tus claves en https://www.themoviedb.org/ */
-const TMDB_API_KEY = '98fee347b91da83932ea8b9daa0edece';
-const TMDB_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGZlZTM0N2I5MWRhODM5MzJlYThiOWRhYTBlZGVjZSIsIm5iZiI6MTYxOTE5NzU3MC4zODIwMDAyLCJzdWIiOiI2MDgyZmU4MjAxYjFjYTAwNDFlYzJlMjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.qqZ5b5Hhv72mOpl8XTTpNi5jtJOc4RMMOF6RExhw69w';
-const TMDB_LANG = 'es';
